29da9bbe0ec3517e652955c8ea74fe349458acb3
cgreenhalgh <chris.greenhalgh@nottingham.ac.uk>
2010-07-05 22:07:49 +0100

    to SDK 1.3.5; output sample mime type as test

--- src/uk/ac/horizon/ug/lobby/LobbyserviceServlet.java
+++ src/uk/ac/horizon/ug/lobby/LobbyserviceServlet.java
@@ -1,12 +1,25 @@
 package uk.ac.horizon.ug.lobby;
 
 import java.io.IOException;
+import java.util.logging.Logger;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class LobbyserviceServlet extends HttpServlet {
+	static Logger logger = Logger.getLogger(LobbyserviceServlet.class.getName());
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		logger.info("Get: contextPath="+req.getContextPath()+", pathInfo="+req.getPathInfo()+", queryString="+req.getQueryString());
+		
+		if ("/json".equals(req.getPathInfo())) {
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("application/x-horizon-ug-lobby-json");
+			resp.getWriter().println("{}");
+			return;
+		}
+		
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
