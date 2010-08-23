dca2f478c15bf0dc23c241e56b0f5f9a9d715aca
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2010-08-23 18:33:48 +0100

    using standard java xml apis for exploding places login

--- src/uk/ac/horizon/ug/lobby/server/ExplodingPlacesServerProtocol.java
+++ src/uk/ac/horizon/ug/lobby/server/ExplodingPlacesServerProtocol.java
@@ -20,6 +20,7 @@
 package uk.ac.horizon.ug.lobby.server;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -32,12 +33,25 @@ import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import uk.ac.horizon.ug.lobby.browser.JoinGameInstanceServlet;
 import uk.ac.horizon.ug.lobby.model.Account;
@@ -57,62 +71,6 @@ import uk.ac.horizon.ug.lobby.protocol.GameJoinResponseStatus;
 public class ExplodingPlacesServerProtocol implements ServerProtocol {
 	static Logger logger = Logger.getLogger(ExplodingPlacesServerProtocol.class.getName());
 
-	static class LoginMessage {
-		/** client ID (IMEI) */
-		String clientId;
-		/** conversation ID */
-		String conversationId;
-		/** player name */
-		String playerName;
-		/** client version */
-		int clientVersion;
-		/** client type */
-		String clientType;
-		/* (non-Javadoc)
-		 * @see java.lang.Object#toString()
-		 */
-		@Override
-		public String toString() {
-			return "LoginMessage [clientId=" + clientId + ", clientType="
-					+ clientType + ", clientVersion=" + clientVersion
-					+ ", conversationId=" + conversationId + ", playerName="
-					+ playerName + "]";
-		}
-	}
-	/** status enum */
-	public static enum Status {
-		NOT_DONE,
-		FAILED,
-		OK, 
-		OLD_CLIENT_VERSION, BAD_CLIENT_VERSION, 
-		UNSUPPORTED_CLIENT_TYPE,
-		GAME_NOT_FOUND, 
-		FORBIDDEN,
-		SERVER_CLOSED
-	};
-	static class LoginReplyMessage {
-		/** game ID (server internal) - for info */
-		private String gameId;
-		/** game status */
-		private String gameStatus;
-		/** status response */
-		private Status status = Status.NOT_DONE;
-		/** message (to user) */
-		private String message;
-		/** detail message */
-		private String detail;
-		/** cons */
-		/* (non-Javadoc)
-		 * @see java.lang.Object#toString()
-		 */
-		@Override
-		public String toString() {
-			return "LoginReplyMessage [detail=" + detail + ", gameId=" + gameId
-					+ ", gameStatus=" + gameStatus + ", message=" + message
-					+ ", status=" + status + "]";
-		}
-		
-	}
 	static final int DEFAULT_TIMEOUT_MS = 30000;
 	@Override
 	public void handlePlayRequest(GameJoinRequest gjreq,
@@ -132,14 +90,18 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 		if (gjresp.getPlayData()==null)
 			gjresp.setPlayData(new HashMap<String,Object>());
 		try {
-			LoginMessage login = new LoginMessage();
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			dbf.setNamespaceAware(false);
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.newDocument();
+			doc.appendChild(doc.createElement("login"));
 			// clientId is meant to be durable across retries/restarts - our clientId is probably OK
-			login.clientId = gc.getId();
+			addElement(doc, "clientId", gc.getId());
 			// conversationId is limited to 20 chars. 
 			// each log should be a new conversationId.
 			gs.setClientSharedSecret(JoinGameInstanceServlet.createClientSharedSecret(20*4));
-			login.conversationId = gs.getClientSharedSecret();
-			gjresp.getPlayData().put("conversationId", login.conversationId);
+			addElement(doc, "conversationId", gs.getClientSharedSecret());
+			gjresp.getPlayData().put("conversationId", gs.getClientSharedSecret());
 			
 			if (gc.getClientType()!=GameClientType.ANDROID) {
 				logger.warning("ExplodingPlaces only supports ANDROID clientType");
@@ -147,10 +109,10 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 				gjresp.setMessage("Sorry - this game only supports Android client(s)");
 				return;
 			}
-			login.clientType= "AndroidDevclient";
-			login.clientVersion= 1;
+			addElement(doc, "clientType", "AndroidDevclient");
+			addElement(doc, "clientVersion", "1");
 			// TODO playerName
-			login.playerName= "lobbyPlayer";
+			addElement(doc, "playerName", "lobbyPlayer");
 
 			if (gi.getBaseUrl()!=null && !gi.getBaseUrl().equals(server.getBaseUrl()))
 				logger.warning("GameInstance baseUrl does not match server baseUrl ("+gi.getBaseUrl()+" vs "+server.getBaseUrl()+") for "+gi);
@@ -163,23 +125,22 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 			conn.setConnectTimeout(DEFAULT_TIMEOUT_MS);
 			conn.setReadTimeout(DEFAULT_TIMEOUT_MS);
 			conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-			logger.info("Send request "+login+" to "+url);
+			logger.info("Send request "+doc+" to "+url);
 			OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
 			// TODO (standard) Xstream doesn't work on GAE :-(
-			XStream xs = new XStream(new PureJavaReflectionProvider());
-			xs.alias("login", LoginMessage.class);
-			xs.toXML(login, osw);
+			Transformer dt = TransformerFactory.newInstance().newTransformer();
+			dt.transform(new DOMSource(doc), new StreamResult(osw));
 			osw.close();
 			int status = conn.getResponseCode();
 			if (status!=HttpServletResponse.SC_OK) 
 				throw new IOException("HTTP response "+status+": "+conn.getResponseMessage());
-			InputStreamReader isr = new InputStreamReader(conn.getInputStream(), "UTF-8");
-			xs.alias("reply", LoginReplyMessage.class);
-			LoginReplyMessage reply = (LoginReplyMessage)xs.fromXML(isr);
-			isr.close();
-			logger.info("Received response "+reply+" from "+url);
-			if (reply.status!=Status.OK) {
-				if (reply.status==Status.FAILED || reply.status==Status.GAME_NOT_FOUND) {
+			InputStream is = conn.getInputStream();
+			doc = db.parse(is);
+			is.close();
+			logger.info("Received response "+doc+" from "+url);
+			String replyStatus = getElement(doc, "status");
+			if (!"OK".equals(replyStatus)) {
+				if ("FAILED".equals(replyStatus) || "GAME_NOT_FOUND".equals(replyStatus)) {
 					logger.warning("Retryable error logging into "+url+": reply");
 					// worth retrying
 					JoinGameInstanceServlet.setTryLater(gjresp);
@@ -187,11 +148,11 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 				}
 				logger.warning("Terminal error logging into "+url+": reply");
 				gjresp.setStatus(GameJoinResponseStatus.ERROR_INTERNAL);
-				gjresp.setMessage("Unable to join game ("+reply.message+")");
+				gjresp.setMessage("Unable to join game ("+getElement(doc, "message")+")");
 				return;
 			}
-			gjresp.getPlayData().put("gameId", reply.gameId);
-			gjresp.getPlayData().put("gameStatus", reply.gameStatus);
+			gjresp.getPlayData().put("gameId", getElement(doc, "gameId"));
+			gjresp.getPlayData().put("gameStatus", getElement(doc, "gameStatus"));
 		} catch (MalformedURLException e) {
 			logger.warning("Problem with request URL based on "+server.getBaseUrl()+": "+e);
 			JoinGameInstanceServlet.setTryLater(gjresp);
@@ -199,6 +160,27 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 		} catch (IOException e) {
 			logger.log(Level.WARNING, "Problem doing login with URL based on "+server.getBaseUrl(), e);
 			JoinGameInstanceServlet.setTryLater(gjresp);
+			return;
+		} catch (ParserConfigurationException e) {
+			logger.log(Level.WARNING, "Problem with XML parser", e);
+			JoinGameInstanceServlet.setError(gjresp, GameJoinResponseStatus.ERROR_INTERNAL, "Problem with ExplodingPlaces protocol");
+			return;
+		} catch (TransformerConfigurationException e) {
+			logger.log(Level.WARNING, "Problem with XML transformer", e);
+			JoinGameInstanceServlet.setError(gjresp, GameJoinResponseStatus.ERROR_INTERNAL, "Problem with ExplodingPlaces protocol");
+			return;
+		} catch (TransformerFactoryConfigurationError e) {
+			logger.log(Level.WARNING, "Problem with XML transformer", e);
+			JoinGameInstanceServlet.setError(gjresp, GameJoinResponseStatus.ERROR_INTERNAL, "Problem with ExplodingPlaces protocol");
+			return;
+		} catch (TransformerException e) {
+			logger.log(Level.WARNING, "Problem with transforming request", e);
+			JoinGameInstanceServlet.setTryLater(gjresp);
+			return;
+		} catch (SAXException e) {
+			logger.log(Level.WARNING, "Problem with parsing request", e);
+			JoinGameInstanceServlet.setTryLater(gjresp);
+			return;
 		}
 
 		// generate client play URL. (Note conversationId is a required parameter)
@@ -206,6 +188,17 @@ public class ExplodingPlacesServerProtocol implements ServerProtocol {
 		gjresp.setPlayUrl(server.getBaseUrl()+"/rpc/messages");
 
 		gjresp.setStatus(GameJoinResponseStatus.OK);
+	}
+	private String getElement(Document doc, String tag) {
+		NodeList els = doc.getDocumentElement().getElementsByTagName(tag);
+		if (els.getLength()==0)
+			return null;
+		return ((Element)els.item(0)).getTextContent();
+	}
+	private void addElement(Document doc, String tag, String value) {
+		Element el = doc.createElement(tag);
+		doc.getDocumentElement().appendChild(el);
+		el.appendChild(doc.createTextNode(value));		
 	}
 
 }
